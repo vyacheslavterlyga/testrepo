cade65c806de6719d0d27f0f967f7805bacbedf4
vyacheslavterlyga <terlygavv@mail.ru>
2016-11-02 16:07:19 +0200

    Security additions

--- spring-mvc/webapp/src/main/java/com/dao/UserDAO.java
+++ spring-mvc/webapp/src/main/java/com/dao/UserDAO.java
@@ -1,31 +1,41 @@
 package com.dao;
 
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.model.User;
 
+
 @Repository
-public class UserDAO {
-	
+public class UserDAO implements UserDetailsService {
 	@Autowired
 	private SessionFactory sessionFactory;
-	
+
 	@Transactional(readOnly = false)
-	public User addUser(User user){
+	public User addUser(User user) {
 		sessionFactory.getCurrentSession().save(user);
-		return user;		
+		return user;
 	}
-	
+
 	@Transactional(readOnly = true)
-	public User getUserById(String id){
-		return (User)sessionFactory.getCurrentSession().createQuery("from com.model.User where id = '" + id + "'").uniqueResult();
+	public User getUserById(String id) {
+		return (User) sessionFactory.getCurrentSession().createQuery("from com.model.User where id = '" + id + "'").uniqueResult();
 	}
-	
+
 	@Transactional(readOnly = true)
-	public User getAllUsers(){
-		return (User)sessionFactory.getCurrentSession().createQuery("from com.model.User where id = ?").uniqueResult();
+	public List <User> getAllUsers() {
+		return sessionFactory.getCurrentSession().createQuery("from com.model.User").list();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
 	}
 }
