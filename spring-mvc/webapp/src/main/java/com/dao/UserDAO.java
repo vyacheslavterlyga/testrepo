1ee7ea56a1578aa82c4d657833dd8714a5297f6f
vyacheslavterlyga <terlygavv@mail.ru>
2016-11-02 23:57:08 +0200

    use hibernat get(id) instead of SQL query

--- spring-mvc/webapp/src/main/java/com/dao/UserDAO.java
+++ spring-mvc/webapp/src/main/java/com/dao/UserDAO.java
@@ -12,30 +12,30 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.model.User;
 
-
 @Repository
 public class UserDAO implements UserDetailsService {
-	@Autowired
-	private SessionFactory sessionFactory;
 
-	@Transactional(readOnly = false)
-	public User addUser(User user) {
-		sessionFactory.getCurrentSession().save(user);
-		return user;
-	}
+  @Autowired
+  private SessionFactory sessionFactory;
 
-	@Transactional(readOnly = true)
-	public User getUserById(String id) {
-		return (User) sessionFactory.getCurrentSession().createQuery("from com.model.User where id = '" + id + "'").uniqueResult();
-	}
+  @Transactional(readOnly = false)
+  public User addUser(User user) {
+    sessionFactory.getCurrentSession().save(user);
+    return user;
+  }
 
-	@Transactional(readOnly = true)
-	public List <User> getAllUsers() {
-		return sessionFactory.getCurrentSession().createQuery("from com.model.User").list();
-	}
+  @Transactional(readOnly = true)
+  public User getUserById(String id) {
+    return (User) sessionFactory.getCurrentSession().get(User.class, id);
+  }
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
-	}
+  @Transactional(readOnly = true)
+  public List<User> getAllUsers() {
+    return sessionFactory.getCurrentSession().createQuery("from com.model.User").list();
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return null;
+  }
 }
