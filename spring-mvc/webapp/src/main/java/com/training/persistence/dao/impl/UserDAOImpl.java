d7851ac41493de806689c611aec9b2341028a217
vyacheslavterlyga <terlygavv@mail.ru>
2016-11-03 19:40:08 +0200

    hibernate entity prefix added userDao fixed web.xml fixed

--- spring-mvc/webapp/src/main/java/com/training/persistence/dao/impl/UserDAOImpl.java
+++ spring-mvc/webapp/src/main/java/com/training/persistence/dao/impl/UserDAOImpl.java
@@ -1,34 +1,27 @@
 package com.training.persistence.dao.impl;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.training.persistence.dao.UserDAO;
+import com.training.persistence.model.User;
 
 public class UserDAOImpl extends AbstractEntityDAOImpl<com.training.persistence.model.User> implements UserDetailsService, UserDAO {
 
   @Override
-  public com.training.persistence.model.User getByLogin(String Login) {
-    return (com.training.persistence.model.User) sessionFactory
-      .getCurrentSession()
-      .createQuery("FROM AUSER WHERE login = :login")
-      .setString("login", Login)
-      .uniqueResult();
+  public User getByLogin(String Login) {
+    return (User) sessionFactory.getCurrentSession().createQuery("FROM User WHERE login = :login").setString("login", Login).uniqueResult();
   }
 
   @Override
   public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-    com.training.persistence.model.User userFromDB = getByLogin(login);
-    String role = userFromDB.getRole();
-    String password = userFromDB.getPassword();
-    ArrayList<GrantedAuthority> authorities = new ArrayList<>();
-    authorities.add(new SimpleGrantedAuthority(role));
-    UserDetails userDetails = new org.springframework.security.core.userdetails.User(login, password, authorities);
-    return userDetails;
+    User userFromDB = getByLogin(login);
+    List<SimpleGrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority(userFromDB.getRole()));
+    return new org.springframework.security.core.userdetails.User(login, userFromDB.getPassword(), authorities);
   }
 }
