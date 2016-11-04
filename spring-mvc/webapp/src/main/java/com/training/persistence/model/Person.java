84c90ce6bb5fe24407695700b80dbe025817a716
vyacheslavterlyga <terlygavv@mail.ru>
2016-11-04 12:26:17 +0200

    person.start_date + person_end_date

--- spring-mvc/webapp/src/main/java/com/training/persistence/model/Person.java
+++ spring-mvc/webapp/src/main/java/com/training/persistence/model/Person.java
@@ -1,27 +1,41 @@
 package com.training.persistence.model;
 
-import javax.persistence.CascadeType;
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "PERSON")
 public class Person extends AbstractEntity {
 
-  @OneToOne(optional = false, cascade = CascadeType.ALL)
-  @JoinColumn(name = "login")
-  @Column(name = "person_id")
-  int id;
-
   @Column(name = "first_name")
+  @Getter
+  @Setter
   String firstName;
 
   @Column(name = "last_name")
+  @Getter
+  @Setter
   String lastName;
 
   @Column(name = "age")
+  @Getter
+  @Setter
   int age;
+
+  @Column(name = "start_date")
+  @Getter
+  @Setter
+  Date startDate;
+
+  @Column(name = "end_date")
+  @Getter
+  @Setter
+  Date endDate;
+
 }
