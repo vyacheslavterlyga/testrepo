aed07d246dd7b75fa6fcd12e37b32960a371e961
vyacheslavterlyga <terlygavv@mail.ru>
2016-11-17 11:10:16 +0200

    exclude package-info generation, move xml adapter annotation into target package

--- spring-mvc/domain-service/src/main/java/com/training/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters({})
-package com.training.service;
---
+++ spring-mvc/domain-service/src/main/java/com/training/service/user/package-info.java
@@ -0,0 +1,3 @@
+@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters({
+  @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(value = com.training.service.adapter.PersonAdapter.class, type = com.training.service.user.Person.class)})
+package com.training.service.user;
